cc09fbd375e5104b5e1fe2f50914f4a14b7eda8f
allysson <allysson@allysson-Aspire-A515-51G>
2018-05-21 21:28:02 -0300

    crud cargo

--- Faculdade02/src/exercicioMer1/cargoDAO.java
+++ Faculdade02/src/exercicioMer1/cargoDAO.java
@@ -2,21 +2,108 @@ package exercicioMer1;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class cargoDAO {
-	
-	public void inserirCargo(cargoVO cargo) throws SQLException {
-		
+
+	public void inserirCargo(cargoVO cargo) {
+
+		try {
+			conexaoBanco con = new conexaoBanco();
+			Connection conexao = con.conectar();
+			String sql = null;
+			sql = ("insert into cargo (car_nome, car_descricao) values (?, ?)");
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, cargo.getNome());
+			stmt.setString(2, cargo.getDescricao());
+			stmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void listarCargo() {
+
 		conexaoBanco con = new conexaoBanco();
 		Connection conexao = con.conectar();
-		String sql = ("insert into cargo (car_nome, car_descricacao) values (?, ?)");
-		PreparedStatement stmt = conexao.prepareStatement(sql);
-		stmt.setString(1, cargo.getNome());
-		stmt.setString(2, cargo.getDescricao());
-		stmt.executeUpdate();
-		conexao.commit();
-		
+		String sql = null;
+		sql = "select * from cargo";
+		Statement stmt;
+		try {
+			stmt = conexao.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println("\nId cargo: " + rs.getInt(1));
+				System.out.println("Nome cargo: " + rs.getString(2));
+				System.out.println("Descrição cargo: " + rs.getString(3) + "\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void buscarCargo(cargoVO cargo) {
+
+		conexaoBanco con = new conexaoBanco();
+		Connection conexao = con.conectar();
+		String sql = null;
+		sql = "select * from cargo where car_nome like '" + cargo.getNome() + "%'";
+		Statement stmt;
+		try {
+			stmt = conexao.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println("\nId cargo: " + rs.getInt(1));
+				System.out.println("Nome cargo: " + rs.getString(2));
+				System.out.println("Descrição cargo: " + rs.getString(3) + "\n");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void excluirCargo(cargoVO cargo) {
+
+		try {
+			conexaoBanco con = new conexaoBanco();
+			Connection conexao = con.conectar();
+			String sql = null;
+			sql = "delete from cargo where car_nome = ?";
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, cargo.getNome());
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void alterarCargo(cargoVO cargo) {
+
+		try {
+			conexaoBanco con = new conexaoBanco();
+			Connection conexao = con.conectar();
+			String sql = null;
+			sql = "update cargo set car_nome = ?, car_descricao = ? where car_id = ?";
+			PreparedStatement stmt = conexao.prepareStatement(sql);
+			stmt.setString(1, cargo.getNome());
+			stmt.setString(2, cargo.getDescricao());
+			stmt.setInt(3, cargo.getIdCargo());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
--- Faculdade02/src/exercicioMer1/mainteste.java
+++ Faculdade02/src/exercicioMer1/mainteste.java
@@ -5,25 +5,40 @@ import java.util.Scanner;
 
 public class mainteste {
 	static Scanner scan = new Scanner (System.in);
+	criaTabelas cria = new criaTabelas();
 	
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		
-		new mainteste().teste();
+		//new mainteste().cria.criacaoTabela();
+		try {
+			new mainteste().teste();
+		} catch (SQLException e) {
+			System.out.println("erro x048146520");
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public void teste() throws SQLException {
 
 		cargoVO cargo = new cargoVO();
-		
+		cargoDAO dao = new cargoDAO();
+		//dao.listarCargo();
+		System.out.println("Informe id: ");
+		cargo.setIdCargo(scan.nextInt());
 		System.out.println("Informe o nome do cargo: ");
 		cargo.setNome(scan.next());
-		System.out.println("Informe a descricao");
+		System.out.println("Informe nova descricao");
 		cargo.setDescricao(scan.next());
-		cargoDAO dao = new cargoDAO();
+		dao.alterarCargo(cargo);
+		//dao.buscarCargo(cargo);
+		/*System.out.println("Informe o nome do cargo: ");
+		cargo.setNome(scan.next());
+		System.out.println("Informe a descricao");
+		cargo.setDescricao(scan.next());		
 		System.out.println(cargo.getNome());
 		System.out.println(cargo.getDescricao());
-		dao.inserirCargo(cargo);
+		dao.inserirCargo(cargo);*/
 	}
 
 }
